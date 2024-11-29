3cbbdd047a083f250d84c3b4660829072d283cab
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-11-29 16:22:09 +0100

    Refactor

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse, HttpStatusCode } from "axios";
+import axios, { HttpStatusCode } from "axios";
 import { SERVER } from "./config";
 import { LoginRequest, Team, User } from "./types.ts";
 
@@ -15,32 +15,32 @@ export const login = async (data: LoginRequest): Promise<User> => {
   });
 
   if (response.status === HttpStatusCode.NoContent) {
-    const user = await instance.get<User>("/me");
-
-    return {
-      id: user.data.id,
-      name: user.data.name,
-      created_at: user.data.created_at,
-      personal_data: {
-        first_name: user.data.personal_data.first_name,
-        last_name: user.data.personal_data.last_name
-      }
-    };
+    throw new Error(response.data.detail);
   }
 
-  throw new Error(response.data.detail);
+  const user = await instance.get<User>("/me");
+
+  return {
+    id: user.data.id,
+    name: user.data.name,
+    created_at: user.data.created_at,
+    personal_data: {
+      first_name: user.data.personal_data.first_name,
+      last_name: user.data.personal_data.last_name
+    }
+  };
 };
 
 export const get_teams = async (): Promise<Array<Team>> => {
-  const response: AxiosResponse<Team[]> = await instance.get("/teams");
+  const response = await instance.get<Team[]>("/teams");
 
-  if (response.status === HttpStatusCode.Ok) {
-    return response.data.map((team) => ({
-      id: team.id,
-      name: team.name,
-      members: team.members,
-    }));
+  if (response.status !== HttpStatusCode.Ok) {
+    throw new Error(response.data.detail);
   }
 
-  throw new Error(response.data.detail);
+  return response.data.map((team: Team) => ({
+    id: team.id,
+    name: team.name,
+    members: team.members,
+  }));
 };
