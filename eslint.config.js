7eb3cf1f973148b352c32b8c6d9138d2804dd768
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-12-01 10:59:01 +0100

    Changed rules

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,8 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
