18e90248b579011da1b53de7e3a0cd3bedb93dce
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-12-09 17:22:44 +0100

    Migrate to i18n

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,7 @@ import { LOCALES } from "../../locales";
 export const HomePage = () => {
   return (
     <div className="flex h-screen">
-      <p className="mx-auto my-auto">{LOCALES.PAGES.HOME.TITLE}</p>
+      <p className="mx-auto my-auto">{t("PAGES.HOME.TITLE")}</p>
     </div>
   );
 };
