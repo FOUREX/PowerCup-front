96dcb718eb58d5aaa83bd4e5aa41298dc0523c56
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-12-09 17:23:35 +0100

    Migrate to i18n

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,8 @@
-import { LOCALES } from "../../locales";
+import {useTranslation} from "react-i18next";
 
 export const HomePage = () => {
+  const {t} = useTranslation()
+
   return (
     <div className="flex h-screen">
       <p className="mx-auto my-auto">{t("PAGES.HOME.TITLE")}</p>
