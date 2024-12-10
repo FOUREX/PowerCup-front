19196faeb50145a888ff2a58ac2a4d80ba1b0166
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-12-10 03:38:42 +0100

    Redesign

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,5 @@
-import { Card, Button, Form, Input } from "antd";
+import {Card, Button, Form, Input, notification} from "antd";
+import {UserOutlined, LockOutlined} from "@ant-design/icons"
 import type { FormProps } from "antd";
 import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router";
@@ -11,70 +12,76 @@ type FieldType = {
 };
 
 export const LoginPage = () => {
-  const { t }: { t: (key: string) => string } = useTranslation()
+  const { t }: { t: (key: string, options?: object) => string } = useTranslation()
+  const [api, contextHolder] = notification.useNotification()
   const navigate = useNavigate()
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
-    const user = await login({
-      login: values.username ?? "",
-      password: values.password ?? "",
-    });
+    try {
+      const user = await login({
+        login: values.username ?? "",
+        password: values.password ?? "",
+      });
 
-    CurrentUser.set(user)
-
-    navigate("/")
-  };
-
-  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+      CurrentUser.set(user);
+      navigate("/");
+    } catch (reason) {
+      api.error({
+        message: t("PAGES.LOGIN.NOTIFICATION.LOGIN_FAILED"),
+        description: reason.message,
+        placement: "bottomRight"
+      })
+    }
   };
 
   return (
-    <div className="flex h-screen">
-      <Card
-        title={t("PAGES.LOGIN.AUTH")}
-        className="mx-auto my-auto"
-      >
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
+    <>
+      {contextHolder}
+      <div className="flex h-screen">
+        <Card
+          title={t("PAGES.LOGIN.AUTH")}
+          className="mx-auto my-auto"
         >
-          <Form.Item<FieldType>
-            label={t("PAGES.LOGIN.USERNAME")}
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: t("PAGES.LOGIN.ENTER_USERNAME"),
-              },
-            ]}
+          <Form
+            name="basic"
+            style={{maxWidth: 600}}
+            initialValues={{remember: true}}
+            onFinish={onFinish}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item<FieldType>
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: t("PAGES.LOGIN.ENTER_USERNAME"),
+                },
+                {
+                  pattern: /^[A-Za-z0-9_]+$/,
+                  message: t("PAGES.LOGIN.INVALID_USERNAME")
+                }
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder={t("PAGES.LOGIN.USERNAME")} />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label={t("PAGES.LOGIN.PASSWORD")}
-            name="password"
-            rules={[
-              { required: true, message: t("PAGES.LOGIN.ENTER_PASSWORD") },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item<FieldType>
+              name="password"
+              rules={[
+                {required: true, message: t("PAGES.LOGIN.ENTER_PASSWORD")},
+              ]}
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder={t("PAGES.LOGIN.PASSWORD")} />
+            </Form.Item>
 
-          <Form.Item label={null}>
-            <Button type="primary" htmlType="submit">
-              {t("PAGES.LOGIN.LOGIN")}
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
+            <Form.Item label={null}>
+              <Button className="w-full" type="primary" htmlType="submit">
+                {t("PAGES.LOGIN.LOGIN")}
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </>
   );
 };
