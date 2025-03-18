cc0cdfa1277411d52e8d6291b533bafc4d19267f
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-03-18 21:15:53 +0100

    Build

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
 const dev = false;
 
-export const SERVER = dev ? "http://127.0.0.1:25565" : "http://91.199.45.219:14088";
+export const SERVER = dev ? "http://127.0.0.1:25565" : "https://91.199.45.219:14088";
